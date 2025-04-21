5a8859bd199e0315012ff33bfb5fc651bb0e2d1f
shin-minhyuk <afs_style@naver.com>
2025-04-21 22:32:59 +0900

    [feat] typeorm 패키지 설치

---
+++ src/movie/entity/movie.entity.ts
@@ -0,0 +1,9 @@
+import { Transform } from 'class-transformer';
+
+export class Movie {
+  id: number;
+  title: string;
+
+  @Transform(({ value }) => value.toString().toUpperCase())
+  genre: string;
+}
