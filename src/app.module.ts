90012e8bde5a4f60e5f2ad89c8627bf8f54afa7d
shin-minhyuk <afs_style@naver.com>
2025-05-19 22:14:29 +0900

    [feat] response time interceptor, transaction interceptor 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,9 +20,10 @@ import { User } from './user/entities/user.entity';
 import { Auth } from './auth/entities/auth.entity';
 import { envVariableKeys } from './common/const/env.const';
 import { BearerTokenMiddleware } from './auth/middleware/bearer-token.middleware';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthGuard } from './auth/guard/auth.guard';
 import { RBACGuard } from './auth/guard/rbac.guard';
+import { ResponseTimeInterceptor } from './common/intercepter/response-time.interceptor';
 
 @Module({
   imports: [
@@ -68,6 +69,10 @@ import { RBACGuard } from './auth/guard/rbac.guard';
     {
       provide: APP_GUARD,
       useClass: RBACGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseTimeInterceptor,
     },
   ],
 })
